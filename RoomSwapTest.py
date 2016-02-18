a528ade2314d337dbc53a1eff974d54899522c75
brad <bradtaniguchi@gmail.com>
2016-02-18 12:53:49 -0800

    PEP8 Standards

--- RoomSwapTest.py
+++ RoomSwapTest.py
@@ -3,21 +3,22 @@
 # Could be simplified, but this way its VERY easy to understand whats going on
 # Easier to impliment back into main program
 
+
 def test(studentinroom1exists, studentinroom2exists):
-    Flag = False  # flag that rooms have been changed
+    flag = False  # flag that rooms have been changed
     if (not studentinroom1exists) or (not studentinroom2exists):  # both are in rooms
         print(">>>" + str(studentinroom1exists) + ">>>" + str(studentinroom2exists))
         print("NO Students in Either Room!")
-        Flag = True
-    if (studentinroom1exists) and (not studentinroom2exists):  # Student In Room1, but no one in Room2
+        flag = True
+    if studentinroom1exists and (not studentinroom2exists):  # Student In Room1, but no one in Room2
         print(">>>" + str(studentinroom1exists) + ">>>" + str(studentinroom2exists))
         print("Moving Student1 into Room2")
-        Flag = True
-    if (not studentinroom1exists) and (studentinroom2exists):  # No one in Room1, but Student in Room2
+        flag = True
+    if (not studentinroom1exists) and studentinroom2exists:  # No one in Room1, but Student in Room2
         print(">>>" + str(studentinroom1exists) + ">>>" + str(studentinroom2exists))
         print("Moving Student2 into Room1")
-        Flag = True
-    if not Flag:  # really the "else"
+        flag = True
+    if not flag:  # really the "else"
         print(">>>" + str(studentinroom1exists) + ">>>" + str(studentinroom2exists))
         print("Swapping Both Rooms")
 
